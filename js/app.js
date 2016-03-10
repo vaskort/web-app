ae9db8a522c723b8012ffd35e15780e490480a4f
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-03-11 00:15:16 +0200

    indentation fix

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@
   var app = angular.module('app', ['ngRoute', 'directive.g+signin', 'ngAnimate', 'app.activitystore', 'xeditable', 'ngSanitize']);
 
   app.config(function($routeProvider) {
-  	$routeProvider
-  		.when('/', {
-  			templateUrl: 'templates/login.html',
-  			controller: 'LoginCtrl'
-  		})
-  		.when('/list', {
-  			templateUrl: 'templates/list.html',
-  			controller: 'GoogleCtrl'
-  		})
-      .when('/activity/:id', {
-  			templateUrl: 'templates/activityDetail.html',
-  			controller: 'activityDetailCtrl'
-  		})
-  		.otherwise({
-  			redirectTo: '/'
-  		});
+    $routeProvider
+    .when('/', {
+      templateUrl: 'templates/login.html',
+      controller: 'LoginCtrl'
+    })
+    .when('/list', {
+      templateUrl: 'templates/list.html',
+      controller: 'GoogleCtrl'
+    })
+    .when('/activity/:id', {
+      templateUrl: 'templates/activityDetail.html',
+      controller: 'activityDetailCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
   });
 
   app.controller('LoginCtrl', function($scope, $location, $http){
@@ -35,59 +35,59 @@
 
   app.controller('GoogleCtrl', function($scope, $http, ActivityStore){
     ActivityStore.list().then(function(response){
-        // $scope.feed = response;
-        console.log(response);
+      // $scope.feed = response;
+      console.log(response);
 
-        // bring the first five items
-        $scope.feed = response.slice(0,5);
+      // bring the first five items
+      $scope.feed = response.slice(0,5);
 
-    		$scope.add = function () {
-    			// length of the first "short" array
-          var length = $scope.feed.length;
-          // push five more when load is clicked
-    			for (var i = length; i < length + 5; i++) {
-    				$scope.feed.push(response[i]);
-    			}
-    		};
+      $scope.add = function () {
+        // length of the first "short" array
+        var length = $scope.feed.length;
+        // push five more when load is clicked
+        for (var i = length; i < length + 5; i++) {
+          $scope.feed.push(response[i]);
+        }
+      };
 
-    		$scope.hideButton = function(){
-    			// if the length of the short array reaches the original array length then hide the button
-    			if ($scope.feed.length === response.length){
-    				return true;
-    			}
-    		}
+      $scope.hideButton = function(){
+        // if the length of the short array reaches the original array length then hide the button
+        if ($scope.feed.length === response.length){
+          return true;
+        }
+      }
 
-        // save function that is triggered when you edit the title
-        $scope.save = function() {
-          ActivityStore.update(response);
-        };
+      // save function that is triggered when you edit the title
+      $scope.save = function() {
+        ActivityStore.update(response);
+      };
 
-      });
+    });
   });
 
   app.controller('activityDetailCtrl', function($scope, $routeParams, ActivityStore) {
-  	$scope.name = "activityDetailCtrl";
-  	ActivityStore.listItem($routeParams.id).then(function(response){
-  		$scope.activityItem = response; //we add activityItem property to scope
-  	});
+    $scope.name = "activityDetailCtrl";
+    ActivityStore.listItem($routeParams.id).then(function(response){
+      $scope.activityItem = response; //we add activityItem property to scope
+    });
   });
 
   // background image directive
   app.directive('bgImg', function(){
-  	return function(scope, element, attrs){
-  		var url = attrs.bgImg;
-  		element.css({
-  			'background-image':'url('+url+')',
-  			'background-size':'cover'
-  		});
-  	};
+    return function(scope, element, attrs){
+      var url = attrs.bgImg;
+      element.css({
+        'background-image':'url('+url+')',
+        'background-size':'cover'
+      });
+    };
   });
 
   // date filter founded it on the web and modified a bit, it adds date suffixes after the day
   app.filter('dateSuffix', function($filter) {
     var suffixes = ["th", "st", "nd", "rd"];
     return function(input) {
-    	if (!input || !input.length) { return; }
+      if (!input || !input.length) { return; }
       var dtfilter = $filter('date')(input, 'dd');
       var dtfilter2 = $filter('date')(input, ' MMM');
       var dtfilter3 = $filter('date')(input, ' yyyy');
